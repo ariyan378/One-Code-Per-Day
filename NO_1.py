9e11d36d1b1a87eed8c085b235c9faff5b865e51
Ariyan Islam hridoy <161193575+ariyan378@users.noreply.github.com>
2025-03-19 04:19:19 +0600

    Add files via upload

---
+++ NO_1.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+arr  = np.array([[1,2,3,4,5,6],
+                 [7,8,9,10,11,12]])
+
+print(np.mean(arr))
+print(np.mean(arr))
+print(np.max(arr))
+print(np.min(arr))
+print(np.std(arr))
+print(np.var(arr))
